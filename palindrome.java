e3583b9e4fa98809a9333f80af7d024b8faac3a7
Ashok <ashok.kumar54@gmail.com>
2026-02-27 23:46:25 +0530

    helloworld

--- palindrome.java
+++ palindrome.java
@@ -1,7 +1,7 @@
 // Palindrome.java
 import java.util.Scanner;
 
-public class Palindrome {
+public class palindrome {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
