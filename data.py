2885fac97b7263457621af26aaba0c8897dae614
ashok-kumar5 <ashok.nikkala54@gmail.com>
2026-05-11 21:00:02 +0530

    python.py

--- data.py
+++ data.py
@@ -1,3 +1,5 @@
 give me all the python programs in python language.
 we are using python to automate the scripts.
 Give me all the data related to python.
+
+Set the project to pem folder and give all the permisssions.
